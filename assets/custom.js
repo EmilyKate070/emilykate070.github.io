f35f45ca1a973e6b5e7b146ad4ba58c0b20260b3
Emily Kate <emily@emilykate.com>
2020-08-07 13:02:54 +0100

    Initial commit

---
+++ assets/custom.js
@@ -0,0 +1,7 @@
+function downClick() {
+    document.getElementById("portfolio").scrollIntoView({behavior: "smooth"});
+}
+
+function upClick() {
+    document.getElementById("landing").scrollIntoView({behavior:"smooth"})
+}
